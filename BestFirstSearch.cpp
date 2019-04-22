208fa736acacde7f7f2605ab567929a019a5d2dd
Grigoriy Redozubov <gredozu1@binghamton.edu>
2019-04-22 09:58:42 -0400

    gross

--- BestFirstSearch.cpp
+++ BestFirstSearch.cpp
@@ -7,7 +7,8 @@
 using namespace std;
 
 vector<item> items; // vector of all items
-vector<item> bestSet; //the best solution we have right now, bestSet[i].include==1 means item i is in the current solution
+vector<int> bestSet; //the best solution we have right now, bestSet[i].include==1 means item i is in the current solution
+int champ; // highest total price so far
 int n; //total amount of items to choose from
 int C; //capacity of knapsack
 
@@ -19,12 +20,14 @@ node root; // root node, for output
 node lastNode; //last node visited, for output
 
 
+
+
 class item{
 	public:
 
 	int w; // weight of item
 	int p; // price of item
-	bool include; // is item included in current solution?
+//	bool include; // is item included in current solution?
 
 };
 
@@ -34,10 +37,20 @@ class node{
 	int profit; //The total money currently in the sack
 	int weight; // the total weight currently in the sack
 	int upperBound; // the total max profit possible to gain at this moment
-
+	int itemNum;
+	vector<int> include; //the items included in this node
 	node * takenptr; // pointer in case next item is taken
 	node * nontakenptr; //pointer in case next item is not taken
 };
+
+
+struct betterThan{
+	bool operator()(node const& n1, node const& n2){
+		return n1.upperBound > n2.upperBound;
+	}
+};
+
+priority_queue<node, vector<node>, betterThan> Q;
 
 
 int KWF(int profit, int weight, int item){
@@ -57,11 +70,12 @@ int KWF(int profit, int weight, int item){
 }
 
 
-node newNode(int item, int profit, int weight, int maxprofit){
+node newNode(int itemNum, int profit, int weight, int maxprofit){
 	node temp; //creates new node to insert into tree
 	temp.profit = profit;
 	temp.weight = weight;
 	temp.upperBound = maxprofit;
+	temp.itemNum = itemNum;
 //	if(weight >= C){
 //		leavesVisited++;
 //	}
@@ -71,12 +85,37 @@ node newNode(int item, int profit, int weight, int maxprofit){
 }
 
 
-void knapsack(int itemi, node nodei){
-	int fractionTaken = KWF(nodei.profit, nodei.weight, itemi); //KWF if current item is put into sack
-	int fractionNont = KWF(nodei.profit, nodei.weight, itemi+1); //KWF if current item is not put into sack
-	nodei.takenptr = newNode( (nodei.profit+items[itemi+1].p) , (nodei.weight+items[itemi+1].w) , fractionTaken);
-	nodei.nontakenptr = newNode( (nodei.profit, nodei.weight, fractionNont);
+void knapsack(node nodei){
+	nodesVisited++;
+	if(weight > C){
+		leavesVisited++;
+	}
+	if(weight <= C &&  nodei.profit > champ){
+		champ = nodei.profit;
+		bestSet = nodei.input;
+	}
+	if(nodei.upperBound < champ){
+		leavesVisited++;
+	}else{
+		int fractionTaken = KWF(nodei.profit, nodei.weight, itemi); //KWF if current item is put into sack
+		int fractionNont = KWF(nodei.profit, nodei.weight, itemi+1); //KWF if current item is not put into sack
+		nodei.takenptr = newNode( (nodei.itemNum+1) ,  (nodei.profit+items[itemi+1].p) , (nodei.weight+items[itemi+1].w) , fractionTaken);
+		nodei.nontakenptr = newNode( (nodei.itemNum+1) , nodei.profit, nodei.weight, fractionNont);
+		nodei.takenptr.include = nodei.include;
+		nodei.nontakenptr.include = nodei.include;
+		nodei.takenptr.include[nodei.itemNum+1] = true;
+		nodei.nontakenptr.include[nodei.itemNum+1] = true;
 
+
+		Q.push(nodei.takenptr);
+		Q.push(nodei.nontakenptr);
+	}
+	if(!Q.empty()){
+		node next; //holds teh values of the next node
+		next = Q.pop();
+		knapsack(next);
+	}
+/*
 	if(fractionNont > fractionTaken){
 		knapsack(itemi+1, nontakenptr);
 		items[itemi+1].include = true;
@@ -88,7 +127,7 @@ void knapsack(int itemi, node nodei){
 		items[itemi+1].include = false;
 		knapsack(itemi+1, nontakenptr);
 	}
-
+*/
 }
 
 
